f12e5bfb89252ee6bcefde08d08cf3684ca325aa
thekinv21 <vadimkiniabaev@gmail.com>
2023-06-14 23:59:33 +0300

    developer tools ve paketler eklendi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,50 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+const constants = {
+	green: '#1db954',
+	'dark-black': '#212121',
+	black: '#121212',
+	white: '#ffffff',
+	'dark-gray': '#535353',
+	gray: '#b3b3b3'
+}
+
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		colors: {
+			transparent: colors.transparent,
+			...constants
+		},
+		extend: {
+			fontSize: {
+				xs: '0.82rem',
+				sm: '0.98rem',
+				base: '1.15rem',
+				lg: '1.22rem',
+				xl: '1.36rem',
+				'1.15xl': '1.5rem',
+				'2xl': '1.725rem',
+				'3xl': '2.155rem',
+				'4xl': '2.58rem',
+				'5xl': '3.45rem',
+				'6xl': '4.3rem',
+				'7xl': '5.17rem',
+				'8xl': '6.9rem',
+				'9xl': '9.2rem'
+			},
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+			}
+		}
+	},
+	plugins: []
 }
