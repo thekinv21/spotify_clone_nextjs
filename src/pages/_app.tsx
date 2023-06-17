5f18a6d9e9feb551d13bae1bcab87aa5c0aae5c2
thekinv21 <vadimkiniabaev@gmail.com>
2023-06-17 22:13:20 +0300

    create providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import '@/styles/globals.css'
 import { ThemeProvider } from '@material-tailwind/react'
 import type { AppProps } from 'next/app'
-import { ToastContainer, Zoom } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+
+import NotificationProvider from '@/provider/NotificationProvider'
+import QueryProviders from '@/provider/QueryProviders'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<ThemeProvider>
-			<Component {...pageProps} />
-			<ToastContainer autoClose={2000} pauseOnHover={false} transition={Zoom} />
-		</ThemeProvider>
+		<QueryProviders>
+			<ThemeProvider>
+				<Component {...pageProps} />
+				<NotificationProvider />
+			</ThemeProvider>
+		</QueryProviders>
 	)
 }
