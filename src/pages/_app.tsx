9034af2c4c34e548b47922467fc4ee5231ca5407
thekinv21 <vadimkiniabaev@gmail.com>
2023-06-19 00:24:48 +0300

    Add redux-toolkit provider for project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,26 @@
 import '@/styles/globals.css'
 import { ThemeProvider } from '@material-tailwind/react'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 
 import NotificationProvider from '@/provider/NotificationProvider'
 import QueryProviders from '@/provider/QueryProviders'
+import ReduxToolkitProvider from '@/provider/ReduxToolkitProvider'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+	Component,
+	pageProps: { session, ...pageProps }
+}: AppProps) {
 	return (
-		<QueryProviders>
-			<ThemeProvider>
-				<Component {...pageProps} />
-				<NotificationProvider />
-			</ThemeProvider>
-		</QueryProviders>
+		<SessionProvider session={session}>
+			<ReduxToolkitProvider>
+				<QueryProviders>
+					<ThemeProvider>
+						<Component {...pageProps} />
+						<NotificationProvider />
+					</ThemeProvider>
+				</QueryProviders>
+			</ReduxToolkitProvider>
+		</SessionProvider>
 	)
 }
