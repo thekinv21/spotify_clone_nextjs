962d78048dac604de823dda23a8306c3f9205cd5
thekinv21 <vadimkiniabaev@gmail.com>
2023-06-21 12:03:17 +0300

    add image config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	images: {
+		domains: ['i.scdn.co', 'mosaic.scdn.co']
+	}
 }
 
 module.exports = nextConfig
