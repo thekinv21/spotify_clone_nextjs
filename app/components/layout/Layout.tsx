38c213efa9aa3a2f27366493db0b06b4726dc1cc
thekinv21 <vadimkiniabaev@gmail.com>
2023-06-23 17:36:17 +0300

    update layout

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -1,3 +1,4 @@
+import styles from '@/scss/Layout.module.scss'
 import { IMeta } from '@/types/IMeta'
 import { FC, PropsWithChildren } from 'react'
 
@@ -5,7 +6,6 @@ import Meta from '@/components/meta/Meta'
 
 import CustomFooter from './footer/CustomFooter'
 import CustomHeader from './header/CustomHeader'
-import styles from './Layout.module.scss'
 import CustomSidebar from './sidebar/CustomSidebar'
 
 const Layout: FC<PropsWithChildren<IMeta>> = ({
@@ -16,15 +16,16 @@ const Layout: FC<PropsWithChildren<IMeta>> = ({
 	return (
 		<Meta title={title} description={description}>
 			<section className={styles.container}>
-				<CustomSidebar>{children}</CustomSidebar>
+				<section className={styles.content}>
+					<CustomSidebar>{children}</CustomSidebar>
 
-				<section className={styles.main}>
-					<CustomHeader />
-
-					<main className={styles.main_content}>{children}</main>
-					<CustomFooter />
+					<section className={styles.main}>
+						<CustomHeader />
+						<main className={styles.main_content}>{children}</main>
+					</section>
 				</section>
 			</section>
+			<CustomFooter />
 		</Meta>
 	)
 }
