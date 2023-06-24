161893da69e4f311341b39b7ce39065422f88547
thekinv21 <vadimkiniabaev@gmail.com>
2023-06-24 18:22:26 +0300

    create spotify discover and ıtems

---
+++ app/components/screens/main/components/spotify_discover/CustomDiscover.tsx
@@ -0,0 +1,33 @@
+import styles from '@/scss/CustomDiscover.module.scss'
+import { FC } from 'react'
+
+import { apiCall } from '@/api/apiCall'
+
+import CustomDiscoverItem from './CustomDiscoverItem'
+
+const CustomDiscover: FC = () => {
+	const {
+		topPlaylist,
+		popPlaylist,
+		followeds,
+		fitnessPlaylist,
+		myPlaylist,
+		dansPlaylist
+	} = apiCall()
+
+	return (
+		<section className={styles.playlist_container}>
+			<CustomDiscoverItem
+				array={topPlaylist}
+				title='En Çok Dinlenen Listeler'
+			/>
+
+			<CustomDiscoverItem array={popPlaylist} title='Pop Müzik' />
+			<CustomDiscoverItem array={fitnessPlaylist} title='Fitness İçin' />
+			<CustomDiscoverItem array={myPlaylist} title='Playlistlerim' />
+			<CustomDiscoverItem array={dansPlaylist} title='Dans ve Elektronik' />
+		</section>
+	)
+}
+
+export default CustomDiscover
